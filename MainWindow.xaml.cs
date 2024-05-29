f935e29693376776838d4eb1ce8c8f3469e7bfa7
Hardik Saxena <teamconnexx@gmail.com>
2024-05-29 11:23:46 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using Auth0.OidcClient;
+using IdentityModel.OidcClient;
+using IdentityModel.OidcClient.Browser;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using IdentityModel.Client;
+using IdentityModel.OidcClient.Results;
+using RestSharp;
+using IRestResponse = RestSharp.Portable.IRestResponse;
+
+namespace WPFSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Auth0Client client;
+
+        readonly string[] _connectionNames = new string[]
+        {
+            "Username-Password-Authentication",
+            "google-oauth2",
+            "twitter",
+            "facebook",
+            "github",
+            "windowslive"
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            string domain = ConfigurationManager.AppSettings["Auth0:Domain"];
+            string clientId = ConfigurationManager.AppSettings["Auth0:ClientId"];
+
+            client = new Auth0Client(new Auth0ClientOptions
+            {
+                Domain = domain,
+                ClientId = clientId,
+                
+                Scope = "openid  email offline_access"
+            });
+
+            var extraParameters = new Dictionary<string, string>();
+            var refreshToken = File.ReadAllText(@"D:\VAOS\RefreshToken.txt");
+
+            extraParameters = new Dictionary<string, string>()
+            {
+                {"prompt","none"},
+                {"refresh_token",refreshToken}
+                // <- According to the article, this should prevent the pop up.
+            };
+
+
+            DisplayResult(await client.LoginAsync(extraParameters));
+        }
+
+
+        private void DisplayResult(LoginResult loginResult)
+        {
+            if (loginResult.IsError)
+            {
+                GetToken();
+
+            }
+            else
+            {
+
+                //Display result
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendLine("Tokens");
+                sb.AppendLine("------");
+                sb.AppendLine($"id_token: {loginResult.IdentityToken}");
+                sb.AppendLine($"access_token: {loginResult.AccessToken}");
+                sb.AppendLine($"refresh_token: {loginResult.RefreshToken}");
+                sb.AppendLine();
+
+                sb.AppendLine("Claims");
+                sb.AppendLine("------");
+                foreach (var claim in loginResult.User.Claims)
+                {
+                    sb.AppendLine($"{claim.Type}: {claim.Value}");
+                }
+
+                resultTextBox.Text = sb.ToString();
+            }
+
+            //Display error
+            //if (loginResult.IsError)
+            //{
+            //    resultTextBox.Text = loginResult.Error;
+            //    return;
+            //}
+
+            logoutButton.Visibility = Visibility.Visible;
+            loginButton.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            connectionNameComboBox.ItemsSource = _connectionNames;
+        }
+
+        private async void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            string domain = ConfigurationManager.AppSettings["Auth0:Domain"];
+            string clientId = ConfigurationManager.AppSettings["Auth0:ClientId"];
+            client = new Auth0Client(new Auth0ClientOptions
+            {
+                Domain = domain,
+                ClientId = clientId,
+                Scope = "profile api_access openid offline_access"
+            });
+
+            BrowserResultType browserResult = await client.LogoutAsync();
+
+            var refreshToken = File.ReadAllText(@"D:\VAOS\RefreshToken.txt");
+            var re =
+                await client.RefreshTokenAsync(
+                    refreshToken);
+
+            var restclient = new RestClient($"https://{domain}/oauth/revoke");
+            var request = new RestRequest(String.Empty,Method.Post);
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", "{ \"client_id\": \"{OERRkQhXgZUHWxyGBEqNaUolaQZsadnF}\", \"client_secret\": \"{F-CFh6JLrnJkwoEc8QJBwK6tK1KqbMLgWhL-DOvBv-Iq3ORnrz-Da-nKZuLCxanJ}\", \"token\": \"{v1.MYeOiIXqSZOTMQkTM3G3hwBfFe13YxNx_zaBSNLGuH4otZh4y7kruuklXnUDDWQrnPA7dLsBfOcFnfmYKHDy1ds}\" }", ParameterType.RequestBody);
+            var response = restclient.Execute(request);
+
+            if (browserResult != BrowserResultType.Success)
+            {
+                resultTextBox.Text = browserResult.ToString();
+                return;
+            }
+
+            logoutButton.Visibility = Visibility.Collapsed;
+            loginButton.Visibility = Visibility.Visible;
+
+            audienceTextBox.Text = "";
+            resultTextBox.Text = "";
+            connectionNameComboBox.ItemsSource = _connectionNames;
+        }
+
+        private async void GetToken()
+        {
+            string domain = ConfigurationManager.AppSettings["Auth0:Domain"];
+            string clientId = ConfigurationManager.AppSettings["Auth0:ClientId"];
+
+
+            client = new Auth0Client(new Auth0ClientOptions
+            {
+                Domain = domain,
+                ClientId = clientId,
+                Scope = "profile api_access openid offline_access"
+            });
+
+            var extraParameters = new Dictionary<string, string>();
+
+            extraParameters = new Dictionary<string, string>()
+            {
+                // <- According to the article, this should prevent the pop up.
+            };
+
+            var refreshToken = File.ReadAllText(@"D:\VAOS\RefreshToken.txt");
+            var re =
+                await client.RefreshTokenAsync(
+                    refreshToken);
+            Thread.Sleep(5000);
+            GetUserInfo(re.AccessToken);
+
+        }
+
+        private async void GetUserInfo(string accesstoken)
+        {
+            string domain = ConfigurationManager.AppSettings["Auth0:Domain"];
+            string clientId = ConfigurationManager.AppSettings["Auth0:ClientId"];
+
+            client = new Auth0Client(new Auth0ClientOptions
+            {
+                Domain = domain,
+                ClientId = clientId,
+                Scope = "openid profile email offline_access"
+            });
+            var user =
+                await client.GetUserInfoAsync(accesstoken);
+            Thread.Sleep(5000);
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("User Info");
+            sb.AppendLine();
+
+            sb.AppendLine("Claims");
+            sb.AppendLine("------");
+            foreach (var claim in user.Claims)
+            {
+                sb.AppendLine($"{claim.Type}: {claim.Value}");
+            }
+
+            resultTextBox.Text = sb.ToString();
+        }
+    }
+}
---
+++ SystemBrowser.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using IdentityModel.OidcClient.Browser;
+
+internal class SystemBrowser : IBrowser
+{
+    const string ERROR_MESSAGE = "Error ocurred.";
+    const string SUCCESSFUL_AUTHENTICATION_MESSAGE = "You have been successfully authenticated. You can now continue to use desktop application.";
+    const string SUCCESSFUL_LOGOUT_MESSAGE = "You have been successfully logged out.";
+    private HttpListener _httpListener;
+    private void StartSystemBrowser(string startUrl)
+    {
+        Process.Start(new ProcessStartInfo(startUrl) { UseShellExecute = true });
+    }
+    public async Task<BrowserResult> InvokeAsync(BrowserOptions options, CancellationToken cancellationToken = default)
+    {
+        StartSystemBrowser(options.StartUrl);
+        var result = new BrowserResult();
+
+        //abort _httpListener if exists
+        _httpListener?.Abort();
+        using (_httpListener = new HttpListener())
+        {
+            var listenUrl = options.EndUrl;
+
+            //HttpListenerContext require uri ends with /
+            if (!listenUrl.EndsWith("/"))
+                listenUrl += "/";
+
+            _httpListener.Prefixes.Add(listenUrl);
+            _httpListener.Start();
+            using (cancellationToken.Register(() =>
+            {
+                _httpListener?.Abort();
+            }))
+            {
+                HttpListenerContext context;
+                try
+                {
+                    context = await _httpListener.GetContextAsync();
+                }
+                //if _httpListener is aborted while waiting for response it throws HttpListenerException exception
+                catch (HttpListenerException)
+                {
+                    result.ResultType = BrowserResultType.UnknownError;
+                    return result;
+                }
+
+
+                //set result response url
+                result.Response = context.Request.Url.AbsoluteUri;
+
+                //generate message displayed in the browser, and set resultType based on request
+                string displayMessage;
+                if (context.Request.QueryString.Get("code") != null)
+                {
+                    displayMessage = SUCCESSFUL_AUTHENTICATION_MESSAGE;
+                    result.ResultType = BrowserResultType.Success;
+                }
+                else if (options.StartUrl.Contains("/logout") && context.Request.Url.AbsoluteUri == options.EndUrl)
+                {
+                    displayMessage = SUCCESSFUL_LOGOUT_MESSAGE;
+
+                    result.ResultType = BrowserResultType.Success;
+                    //var storedCookie = context.Request.Cookies[0];
+                    //var cookie = new Cookie(storedCookie.Name, "");
+                    //cookie.Expires = DateTime.Now.AddDays(-1);
+                    //context.Response.SetCookie(cookie);
+                }
+                else
+                {
+                    displayMessage = ERROR_MESSAGE;
+                    result.ResultType = BrowserResultType.UnknownError;
+                }
+
+
+                //return message to be displayed in the browser
+                Byte[] buffer = System.Text.Encoding.UTF8.GetBytes(displayMessage);
+                context.Response.ContentLength64 = buffer.Length;
+                context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+
+                context.Response.OutputStream.Close();
+                context.Response.Close();
+                _httpListener.Stop();
+            }
+        }
+        return result;
+    }
+
+    private async void Cookie()
+    {
+
+        CookieContainer cookieContainer = new CookieContainer();
+
+        // Create a request to localhost
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost:3000");
+
+        // Assign the CookieContainer to the request
+        request.CookieContainer = cookieContainer;
+
+        // Send the request and get the response
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        // Retrieve cookies for localhost from the CookieContainer
+        CookieCollection cookies = cookieContainer.GetCookies(new Uri("http://localhost:8888"));
+
+        foreach (Cookie cookie in cookies)
+        {
+            Console.WriteLine("Name: " + cookie.Name);
+            Console.WriteLine("Value: " + cookie.Value);
+            Console.WriteLine("Domain: " + cookie.Domain);
+            Console.WriteLine();
+        }
+
+        // Close the response
+        response.Close();
+    }
+}
